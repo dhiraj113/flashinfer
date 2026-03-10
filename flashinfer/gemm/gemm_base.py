fa549c758a5a3a292c2e0742db4d490312205059
Dhiraj Reddy Nallapa <dnallapa@nvidia.com>
2026-03-09 18:19:59 -0700

    tiny

--- flashinfer/gemm/gemm_base.py
+++ flashinfer/gemm/gemm_base.py
@@ -22,6 +22,8 @@ from typing import List, Literal, Optional, Tuple
 from flashinfer.trtllm_low_latency_gemm import trtllm_low_latency_gemm
 import torch
 
+import os
+
 from ..api_logging import flashinfer_api
 from ..autotuner import (
     AutoTuner,
@@ -1732,7 +1734,11 @@ def _get_cudnn_handle(device, stream: torch.cuda.Stream):
     if _cudnn_handles.get(device_id) is None:
         _check_cudnn_availability()
         _cudnn_handles[device_id] = cudnn.create_handle()
-        print("cudnn_handle created for device_id = {}\n".format(device_id))
+        print(
+            "(PID:{} Parent PID:{} - cudnn_handle created for device_id = {}\n".format(
+                device_id, os.getpid(), os.getppid()
+            )
+        )
     cudnn.set_stream(_cudnn_handles[device_id], stream.cuda_stream)
 
     return _cudnn_handles[device_id]
